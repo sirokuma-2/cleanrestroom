19d38e3ce3a6a15807be667c464df40acd6bcb06
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-02-18 06:36:35 +0900

    環境変数の読込設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,17 @@ class PostsController < ApplicationController
   def top
   end
   def index
+    gon.googlemap_key = ENV['GOOGLE_MAP_KEY']
+    @posts = Post.includes(:facility).all
+    gon.posts = @posts.map do |post|
+      {
+        id: post.id,
+        name: post.facility.name,
+        address: post.facility.address,
+        content: post.facility.content,
+        latitude: post.facility.latitude,
+        longitude: post.facility.longitude
+      }
+    end
   end
 end
