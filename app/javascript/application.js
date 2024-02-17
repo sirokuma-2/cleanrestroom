2ff4e9c4177ad6a26ba823a92f6f727ac0d44b98
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-02-18 05:25:23 +0900

    importmap.rb application.jsの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+pin "index", to:"index.js"
+pin "addMarkers", to:"addMarkers.js"
+pin "calculateAndDisplayRoute", to:"calculateAndDisplayRoute.js"
+pin "clearMarkers", to:"clearMarkers.js"
+pin "clickListener", to:"clickListener.js"
+pin "handleLocationError.js", to:"handleLocationError.js"
