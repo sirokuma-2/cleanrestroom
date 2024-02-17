ce7887ec4b0d07b9924c1f19b8f7e5631e4010c1
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-02-18 06:49:22 +0900

    index.jsの修正 マップの表示

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,1 +1,99 @@
-console.log("test");
+// import { clickListener } from "./clickListener.js";
+import { addMarkers } from "./addMarkers.js";
+// import { handleLocationError } from "./handleLocationError.js";
+
+let map;
+
+let currentLanguage = "ja"; // デフォルトの言語を設定
+
+let locations; //すべての施設の位置
+let allMarkers = []; // すべてのマーカーを保持する配列
+
+let userPos; // ユーザーの現在位置
+
+let directionsService; // ルートを検索するためのDirectionsServiceのインスタンス
+let directionsRenderer; // マップ上にルートを表示するためのDirectionsRendererのインスタンス
+
+//非同期関数　マップの初期化
+async function initMap() {
+  //Google Mapsライブラリを非同期にインポート　　Mapクラス
+  const { Map } = await google.maps.importLibrary("maps");
+
+  // gonから施設の位置情報を取得
+  locations = gon.locations;
+
+  //ルート検索機能
+  directionsService = new google.maps.DirectionsService(); //ルートを検索するためのインスタンス
+  directionsRenderer = new google.maps.DirectionsRenderer(); //マップにルートを表示するためのインスタンス
+
+  // Mapクラスからインスタンスを作成　マップを表示　　HTMLのid="map"にマップを挿入
+  map = new Map(document.getElementById("map"), {
+    zoom: 13,
+    mapId: "DEMO_MAP_ID",
+    maxZoom: 18,
+  });
+
+  // GPSで現在地を表示　現在地にhouseアイコンを表示　マーカーと詳細情報を表示
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        userPos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        const mapElement = document.getElementById("map");
+        const houseIconUrl = mapElement.getAttribute("data-house-icon-url");
+
+        //現在地のアイコンの設定
+        const userIcon = {
+          url: houseIconUrl,
+          scaledSize: new google.maps.Size(20, 20),
+          origin: new google.maps.Point(0, 0),
+        };
+
+        //現在地にhouseのアイコンを表示
+        new google.maps.Marker({
+          position: userPos,
+          map: map,
+          title: "Your Location",
+          icon: userIcon,
+        });
+
+        // マップの中心を現在位置に移動
+        map.setCenter(userPos);
+
+        // マーカーと詳細情報を表示
+        // addMarkers(
+        //   locations,
+        //   map,
+        //   allMarkers,
+        //   userPos,
+        //   directionsService,
+        //   directionsRenderer
+        // );
+      },
+      () => {
+        //handleLocationErrorの設定
+        // handleLocationError(true, map.getCenter());
+      }
+    );
+  } else {
+    //handleLocationErrorの設定
+    // handleLocationError(false, map.getCenter());
+  }
+
+  //右クリックの拡張
+  google.maps.event.addListener(map, "rightclick", (event) => {
+    // clickListener 関数を呼び出し、マーカーを作成
+    // const marker = clickListener(event, map);
+
+    // マーカーに 'click' イベントリスナーを追加してマーカーを削除
+    google.maps.event.addListener(marker, "click", () => {
+      marker.setMap(null); // マーカーをマップから削除
+    });
+  });
+}
+
+//地図の表示を実行
+initMap();
