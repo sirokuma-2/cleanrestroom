2302434eccab7647421dfab549f7e9a9ada8975c
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-02-27 05:39:26 +0900

    API読み込みタイミング設定

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -14,53 +14,6 @@ let userPos; // ユーザーの現在位置
 let directionsService; // ルートを検索するためのDirectionsServiceのインスタンス
 let directionsRenderer; // マップ上にルートを表示するためのDirectionsRendererのインスタンス
 
-const googleMapKey = gon.googlemap_key;
-
-//Google Map APIのロード
-function loadGoogleMapsAPI() {
-  ((g) => {
-    var h,
-      a,
-      k,
-      p = "The Google Maps JavaScript API",
-      c = "google",
-      l = "importLibrary",
-      q = "__ib__",
-      m = document,
-      b = window;
-    b = b[c] || (b[c] = {});
-    var d = b.maps || (b.maps = {}),
-      r = new Set(),
-      e = new URLSearchParams(),
-      u = () =>
-        h ||
-        (h = new Promise(async (f, n) => {
-          await (a = m.createElement("script"));
-          e.set("libraries", [...r] + "");
-          for (k in g)
-            e.set(
-              k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
-              g[k]
-            );
-          e.set("callback", c + ".maps." + q);
-          a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
-          d[q] = f;
-          a.onerror = () => (h = n(Error(p + " could not load.")));
-          a.nonce = m.querySelector("script[nonce]")?.nonce || "";
-          m.head.append(a);
-        }));
-    //APIをすでに読み込んでいる場合再度読み込まない
-    if (!d[l]) {
-      d[l]
-        ? console.warn(p + " only loads once. Ignoring:", g)
-        : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
-    }
-  })({
-    key: googleMapKey,
-    v: "beta",
-  });
-}
-
 //非同期関数　マップの初期化
 async function initMap() {
   //Google Mapsライブラリを非同期にインポート　　Mapクラス
@@ -194,6 +147,5 @@ async function initMap() {
 }
 
 document.addEventListener("turbo:load", function () {
-  loadGoogleMapsAPI(); // APIのロード
   initMap(); // マップの初期化関数をここで呼び出す
 });
