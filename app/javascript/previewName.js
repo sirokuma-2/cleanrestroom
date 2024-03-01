2a5c3206a2aec1becd14add366d2fb35399c27e8
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-02 08:45:45 +0900

    previewName.jsの修正

--- app/javascript/previewName.js
+++ app/javascript/previewName.js
@@ -2,7 +2,7 @@ document.addEventListener("turbo:load", function () {
   const fileFieldName = document.querySelector(
     'input[type="file"][name="user[imageName]"]'
   );
-  const previewList = document.getElementById("user_imageName");
+  const previewList = document.getElementById("preview-name");
 
   if (fileFieldName) {
     fileFieldName.addEventListener("change", function (e) {
