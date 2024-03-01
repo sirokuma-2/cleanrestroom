3ff38980a0cb1aff01e156c01cae3ed9fc2611a0
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-01 23:42:52 +0900

    現在地ボタンを作成

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -42,108 +42,124 @@ async function initMap() {
     });
   }
 
+  //現在地を取得するボタン プライバシー確保のため現在地をいきなり表示できない
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "現在地を表示";
+
+  // ボタンをページに追加
+  locationButton.style.backgroundColor = "#FFFFFF";
+  locationButton.style.color = "#000000";
+  locationButton.style.fontSize = "16px";
+  locationButton.style.border = "none";
+  locationButton.style.padding = "10px 20px";
+  locationButton.style.marginTop = "10px";
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
   // GPSで現在地を表示　現在地にhouseアイコンを表示　マーカーと詳細情報を表示
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        userPos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
+  locationButton.addEventListener("click", function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          userPos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
 
-        const mapElement =
-          document.getElementById("top-map") || document.getElementById("map");
+          const mapElement =
+            document.getElementById("top-map") ||
+            document.getElementById("map");
 
-        const houseIconUrl = mapElement.getAttribute("data-house-icon-url");
-        const restroomIconUrl = mapElement.getAttribute(
-          "data-restroom-icon-url"
-        );
-        const routeIconUrl = mapElement.getAttribute("data-route-icon-url");
+          const houseIconUrl = mapElement.getAttribute("data-house-icon-url");
+          const restroomIconUrl = mapElement.getAttribute(
+            "data-restroom-icon-url"
+          );
+          const routeIconUrl = mapElement.getAttribute("data-route-icon-url");
 
-        //現在地のアイコンの設定
-        // const userIcon = {
-        //   url: houseIconUrl,
-        //   scaledSize: new google.maps.Size(20, 20),
-        //   origin: new google.maps.Point(0, 0),
-        // };
+          //現在地のアイコンの設定
+          // const userIcon = {
+          //   url: houseIconUrl,
+          //   scaledSize: new google.maps.Size(20, 20),
+          //   origin: new google.maps.Point(0, 0),
+          // };
 
-        //現在地にhouseのアイコンを表示
-        new google.maps.marker.AdvancedMarkerView({
-          position: userPos,
-          map: map,
-          title: "Your Location",
-          // icon: userIcon,
-        });
+          //現在地にhouseのアイコンを表示
+          new google.maps.marker.AdvancedMarkerView({
+            position: userPos,
+            map: map,
+            title: "Your Location",
+            // icon: userIcon,
+          });
 
-        // マップの中心を現在位置に移動
-        map.setCenter(userPos);
+          // マップの中心を現在位置に移動
+          map.setCenter(userPos);
 
-        // マーカーと詳細情報を表示
-        addMarkers(
-          locations,
-          map,
-          allMarkers,
-          userPos,
-          directionsService,
-          directionsRenderer,
-          restroomIconUrl,
-          routeIconUrl
-        );
-      },
-      () => {
-        //handleLocationErrorの設定
-        // handleLocationError(true, map.getCenter());
+          // マーカーと詳細情報を表示
+          addMarkers(
+            locations,
+            map,
+            allMarkers,
+            userPos,
+            directionsService,
+            directionsRenderer,
+            restroomIconUrl,
+            routeIconUrl
+          );
+        },
+        () => {
+          //handleLocationErrorの設定
+          // handleLocationError(true, map.getCenter());
+          const mapElement =
+            document.getElementById("top-map") ||
+            document.getElementById("map");
+          const houseIconUrl = mapElement.getAttribute("data-house-icon-url");
+          const restroomIconUrl = mapElement.getAttribute(
+            "data-restroom-icon-url"
+          );
+          const routeIconUrl = mapElement.getAttribute("data-route-icon-url");
 
-        const mapElement =
-          document.getElementById("top-map") || document.getElementById("map");
-        const houseIconUrl = mapElement.getAttribute("data-house-icon-url");
-        const restroomIconUrl = mapElement.getAttribute(
-          "data-restroom-icon-url"
-        );
-        const routeIconUrl = mapElement.getAttribute("data-route-icon-url");
+          //現在地のアイコンの設定
+          const userIcon = {
+            url: houseIconUrl,
+            scaledSize: new google.maps.Size(20, 20),
+            origin: new google.maps.Point(0, 0),
+          };
 
-        //現在地のアイコンの設定
-        const userIcon = {
-          url: houseIconUrl,
-          scaledSize: new google.maps.Size(20, 20),
-          origin: new google.maps.Point(0, 0),
-        };
+          // 現在地の取得に失敗した場合の処理
+          // 東京駅の座標
+          const tokyoStationPos = {
+            lat: 35.681236,
+            lng: 139.767125,
+          };
 
-        // 現在地の取得に失敗した場合の処理
-        // 東京駅の座標
-        const tokyoStationPos = {
-          lat: 35.681236,
-          lng: 139.767125,
-        };
+          // 東京駅にマーカーを設置（オプション）
+          new google.maps.marker.AdvancedMarkerView({
+            position: tokyoStationPos,
+            map: map,
+            title: "Tokyo Station",
+            // icon: userIcon,
+          });
 
-        // 東京駅にマーカーを設置（オプション）
-        new google.maps.marker.AdvancedMarkerView({
-          position: tokyoStationPos,
-          map: map,
-          title: "Tokyo Station",
-          // icon: userIcon,
-        });
+          // マップの中心を現在位置に移動
+          map.setCenter(tokyoStationPos);
 
-        // マップの中心を現在位置に移動
-        map.setCenter(tokyoStationPos);
-
-        // マーカーと詳細情報を表示
-        addMarkers(
-          locations,
-          map,
-          allMarkers,
-          tokyoStationPos,
-          directionsService,
-          directionsRenderer,
-          restroomIconUrl,
-          routeIconUrl
-        );
-      }
-    );
-  } else {
-    //handleLocationErrorの設定
-    handleLocationError(false, map.getCenter());
-  }
+          // マーカーと詳細情報を表示
+          addMarkers(
+            locations,
+            map,
+            allMarkers,
+            tokyoStationPos,
+            directionsService,
+            directionsRenderer,
+            restroomIconUrl,
+            routeIconUrl
+          );
+        }
+      );
+    } else {
+      //handleLocationErrorの設定
+      handleLocationError(false, map.getCenter());
+    }
+  });
 
   //右クリックの拡張
   google.maps.event.addListener(map, "rightclick", (event) => {
