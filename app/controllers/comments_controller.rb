4f787edc96d61c802949451a8659aa1cdc2585eb
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-03 04:48:07 +0900

    postコントローラーの修正 commentコントローラーの修正 posts/show.html.erbの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,4 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
   def create
     comment = Comment.create(comment_params)
     redirect_to "/posts/#{comment.post.id}"  # コメントと結びつくツイートの詳細画面に遷移する
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#top'
   resources :posts do
-    resources :comments, only: [:index, :create]
+    resources :comments, only: [:create]
   end
   resources :users, only: [:show, :edit, :update]
 end
