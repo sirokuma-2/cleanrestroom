cb5fc2feb0a21185204697a0857c2a325554ae08
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-02 15:33:33 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    passwords: 'public/passwords'
+  }
   root to: 'posts#top'
   resources :posts
   resources :users, only: [:show, :edit, :update]
