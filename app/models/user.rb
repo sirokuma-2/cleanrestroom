d1016cd1411cfebeb852c2fbfc513689b28b2edb
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-03 04:23:51 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :recoverable
   has_many :posts
+  has_many :comments
   has_one_attached :imageName
 end
