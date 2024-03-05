2a2c396fcafddedb8970237bc11fbfc2b2a6f3d3
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-05 20:30:59 +0900

    tagモデル posttagモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_02_230616) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_05_112512) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,12 +58,27 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_02_230616) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "post_tags", charset: "utf8", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
   create_table "posts", charset: "utf8", force: :cascade do |t|
     t.bigint "facility_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["facility_id"], name: "index_posts_on_facility_id"
+  end
+
+  create_table "tags", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -81,5 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_02_230616) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "facilities"
 end
