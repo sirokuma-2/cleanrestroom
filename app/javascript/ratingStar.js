7f0b08863ea2f3fbacf4d72c1cddbc0a96b3013d
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-08 05:30:32 +0900

    レビュー星が無限に増えないための修正

--- app/javascript/ratingStar.js
+++ app/javascript/ratingStar.js
@@ -2,12 +2,16 @@ import $ from "jquery";
 
 $(document).on("turbo:load", function () {
   let elem = document.querySelector("#star-rating");
-  let opt = {
-    starOn: "asset_path('star-on.png') ",
-    starOff: "asset_path('star-off.png') ",
-    starHalf: "asset_path('star-half.png') ",
-    readOnly: true,
-    score: elem.getAttribute("data-score"),
-  };
-  raty(elem, opt);
+  elem.setAttribute("data-raty-initialized", "true"); //星が増えないための属性
+  if (elem && !elem.hasAttribute("data-raty-initialized")) {
+    let opt = {
+      starOn: "asset_path('star-on.png') ",
+      starOff: "asset_path('star-off.png') ",
+      starHalf: "asset_path('star-half.png') ",
+      readOnly: true,
+      score: elem.getAttribute("data-score"),
+    };
+
+    raty(elem, opt);
+  }
 });
