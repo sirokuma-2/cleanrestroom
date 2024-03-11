680828320930faefcb20ccc5fb69b111ed3d5126
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-12 05:03:19 +0900

    model/user.rbの修正 user_spec.rbの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :recoverable
+
+  validates :name, presence: true
+
   has_many :posts
   has_many :comments
   has_one_attached :imageName
