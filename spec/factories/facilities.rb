cece778bba4ed16a21de5cddbee3d21e961a62c1
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-12 04:54:56 +0900

    factoryファイルの修正

--- spec/factories/facilities.rb
+++ spec/factories/facilities.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :facility do
     name { Faker::Address.city }
-    address { Faker::Address.street_address }
+    address { "東京都" + Faker::Address.city }
     content {Faker::Lorem.sentence}
     latitude {Faker::Number.between(from: 35.681236 - 0.1, to: 35.681236 + 0.1)}
     longitude {Faker::Number.between(from: 139.767125 - 0.1, to: 139.767125 + 0.1)}
@@ -10,5 +10,9 @@ FactoryBot.define do
     diaper_changing_station { Faker::Number.between(from: 0, to: 1) }
     powder_corner { Faker::Number.between(from: 0, to: 1) }
     stroller_accessible { Faker::Number.between(from: 0, to: 1) }
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
