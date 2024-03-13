0e76f6d64a509a94ea7e4927bdae24e72a1d7655
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-14 06:01:05 +0900

    コメント一人一回制限

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  validates :user_id, uniqueness: { scope: :post_id , message: "コメントは一人１回である必要があります"}
   validates  :rating, presence: true
 end
