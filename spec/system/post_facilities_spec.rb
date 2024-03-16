686e45115c1ac173973bf213297fb41818c1415b
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-17 07:19:11 +0900

    ログイン機能のモジュール

--- spec/system/post_facilities_spec.rb
+++ spec/system/post_facilities_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "PostFacilities", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
   end
+  context 'ツイート投稿ができるとき'do
+  it 'ログインしたユーザーは新規登録できる' do
+    # ログインする
+    sign_in(@user)
+    # 新規投稿ページへのボタンがあることを確認する
+    # 投稿ページに移動する
+    # フォームに情報を入力する
+    # 送信するとTweetモデルのカウントが1上がることを確認する
+    # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
+    # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
+  end
+end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
