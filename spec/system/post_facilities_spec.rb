4b9d38c3aefd06322c60356d731ec8711dfd2220
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-17 14:55:42 +0900

    ログインしたユーザーは新規登録できる(画像あり)

--- spec/system/post_facilities_spec.rb
+++ spec/system/post_facilities_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "PostFacilities", type: :system do
   before do
     @user =  FactoryBot.create(:user)
+    @user2 =  FactoryBot.create(:user)
     @facility = FactoryBot.create(:facility)
     @post_facility = FactoryBot.build(:post_facility,
                                        user_id: @user.id,
@@ -21,42 +22,186 @@ RSpec.describe "PostFacilities", type: :system do
                                        )
     end
 
-  context '新規投稿ができるとき'do
-  it 'ログインしたユーザーは新規登録できる' do
-    # ログインする
-    sign_in(@user)
-    # 投稿ページに移動する
-    visit "#{new_post_path}?latitude=#{@post_facility.latitude}&longitude=#{@post_facility.longitude}"
-    sleep 1
-    # フォームに情報を入力する
-    fill_in '名称', with: @post_facility.name
-    fill_in '住所', with: @post_facility.address
-    fill_in 'コメント', with: @post_facility.content
-    @post_facility.nursing_room == true ? check('授乳室') : uncheck('授乳室')
-    @post_facility.anyone_toilet == true ? check('誰でもトイレ') : uncheck('誰でもトイレ')
-    @post_facility.diaper_changing_station == true ? check('オムツ交換台') : uncheck('オムツ交換台')
-    @post_facility.powder_corner == true ? check('パウダーコーナー') : uncheck('パウダーコーナー')
-    @post_facility.stroller_accessible == true ? check('ベビーカー可') : uncheck('ベビーカー可')
+#   context '新規投稿ができるとき'do
+#     it 'ログインしたユーザーは新規登録できる' do
+#       # ログインする
+#       sign_in(@user)
+#       # 投稿ページに移動する
+#       visit "#{new_post_path}?latitude=#{@post_facility.latitude}&longitude=#{@post_facility.longitude}"
+#       sleep 1
+#       # フォームに情報を入力する
+#       fill_in '名称', with: @post_facility.name
+#       fill_in '住所', with: @post_facility.address
+#       fill_in 'コメント', with: @post_facility.content
+#       @post_facility.nursing_room == true ? check('授乳室') : uncheck('授乳室')
+#       @post_facility.anyone_toilet == true ? check('誰でもトイレ') : uncheck('誰でもトイレ')
+#       @post_facility.diaper_changing_station == true ? check('オムツ交換台') : uncheck('オムツ交換台')
+#       @post_facility.powder_corner == true ? check('パウダーコーナー') : uncheck('パウダーコーナー')
+#       @post_facility.stroller_accessible == true ? check('ベビーカー可') : uncheck('ベビーカー可')
 
 
 
-    # image_path = Rails.root.join('public/images/test_image.png')
+#       image_path = Rails.root.join('public/images/test_image.png')
 
-    # # # 画像選択フォームに画像を添付する
-    # attach_file("post_facility[image]", image_path, make_visible: true)
+#       # # # 画像選択フォームに画像を添付する
+#       attach_file("post_facility[image]", image_path, make_visible: true)
 
-    # 送信するとPostモデルのカウントが1上がることを確認する
+#       # 送信するとPostモデルのカウントが1上がることを確認する
 
-    find('input[name="commit"]').click
-    sleep 5
+#       expect{
+#       find('input[name="commit"]').click
+#       sleep 1
+#     }.to change {Post.count }.by(1)
 
-    # #最新のpostidを取得
-    latest_post_id= Post.last.id
-    puts latest_post_id
-    visit post_path(latest_post_id)
-    # 投稿した内容の画像が存在することを確認する（画像）
-    # 先ほど投稿した内容の名前が存在することを確認する（テキスト）
-    expect(page).to have_content("#{@post_facility.name}")
+#       # #最新のpostidを取得
+#       latest_post_id= Post.last.id
+#       visit post_path(latest_post_id)
+#       # 投稿した内容の画像が存在することを確認する（画像）
+#       expect(page).to have_xpath("//img[contains(@src, 'test_image')]")
+#       # 先ほど投稿した内容の名前が存在することを確認する（テキスト）
+#       expect(page).to have_content("#{@post_facility.name}")
+#     end
+#   end
+
+#  context '新規投稿ができないとき'do
+#     it 'ログインしていないと新規投稿ページに遷移できない' do
+#       # トップページに遷移する
+#       visit root_path
+#       # 投稿ページに移動する
+#       visit "#{new_post_path}?latitude=#{@post_facility.latitude}&longitude=#{@post_facility.longitude}"
+#       expect(page).to have_content("ログイン")
+#     end
+#  end
+
+
+#  context '投稿を編集できるとき'do
+#     it 'ログインしたユーザーは自分が投稿した施設情報の編集ができる' do
+#        # ログインする
+#        sign_in(@user)
+#        # 投稿ページに移動する
+#        visit "#{new_post_path}?latitude=#{@post_facility.latitude}&longitude=#{@post_facility.longitude}"
+#        sleep 1
+#        # フォームに情報を入力する
+#        fill_in '名称', with: @post_facility.name
+#        fill_in '住所', with: @post_facility.address
+#        fill_in 'コメント', with: @post_facility.content
+#        @post_facility.nursing_room == true ? check('授乳室') : uncheck('授乳室')
+#        @post_facility.anyone_toilet == true ? check('誰でもトイレ') : uncheck('誰でもトイレ')
+#        @post_facility.diaper_changing_station == true ? check('オムツ交換台') : uncheck('オムツ交換台')
+#        @post_facility.powder_corner == true ? check('パウダーコーナー') : uncheck('パウダーコーナー')
+#        @post_facility.stroller_accessible == true ? check('ベビーカー可') : uncheck('ベビーカー可')
+
+
+
+#        image_path = Rails.root.join('public/images/test_image.png')
+
+#        # # # 画像選択フォームに画像を添付する
+#        attach_file("post_facility[image]", image_path, make_visible: true)
+
+#        # 送信するとPostモデルのカウントが1上がることを確認する
+
+#        expect{
+#        find('input[name="commit"]').click
+#        sleep 1
+#      }.to change {Post.count }.by(1)
+
+#        # #最新のpostidを取得
+#        latest_post_id= Post.last.id
+#        visit edit_post_path(latest_post_id)
+
+#        # すでに投稿済みの内容がフォームに入っていることを確認する
+#        expect(page).to have_xpath("//img[contains(@src, 'test_image')]")
+#        expect(page).to have_field('名称', with: @post_facility.name)
+#        expect(page).to have_field('住所', with: @post_facility.address)
+#        expect(page).to have_field('コメント', with: @post_facility.content)
+
+#        # 投稿内容を編集する
+#        fill_in '名称', with: "#{@post_facility.name}+編集した名称"
+#        fill_in '住所', with: "#{@post_facility.address}+編集した住所"
+#        fill_in 'コメント', with: "#{@post_facility.content}+編集したコメント"
+
+#        image_path2 = Rails.root.join('public/images/test_image2.png')
+
+#        # # # 画像選択フォームに画像を添付する
+#        attach_file("facility[image]", image_path2, make_visible: true)
+
+
+#        # 編集してもTweetモデルのカウントは変わらないことを確認する
+#        expect{
+#         find('input[name="commit"]').click
+#         sleep 1
+#       }.to change { Post.count }.by(0)
+
+#       visit post_path(latest_post_id)
+#       expect(page).to have_xpath("//img[contains(@src, 'test_image2')]")
+#       expect(page).to have_content("#{@post_facility.name}+編集した名称")
+#     end
+#  end
+ context '投稿を編集できるとき'do
+  it 'ログインしたユーザーは自分以外の投稿した施設情報の編集画面にはせにできない' do
+      # ログインする
+      sign_in(@user)
+      # 投稿ページに移動する
+      visit "#{new_post_path}?latitude=#{@post_facility.latitude}&longitude=#{@post_facility.longitude}"
+      sleep 1
+      # フォームに情報を入力する
+      fill_in '名称', with: @post_facility.name
+      fill_in '住所', with: @post_facility.address
+      fill_in 'コメント', with: @post_facility.content
+      @post_facility.nursing_room == true ? check('授乳室') : uncheck('授乳室')
+      @post_facility.anyone_toilet == true ? check('誰でもトイレ') : uncheck('誰でもトイレ')
+      @post_facility.diaper_changing_station == true ? check('オムツ交換台') : uncheck('オムツ交換台')
+      @post_facility.powder_corner == true ? check('パウダーコーナー') : uncheck('パウダーコーナー')
+      @post_facility.stroller_accessible == true ? check('ベビーカー可') : uncheck('ベビーカー可')
+
+
+
+      image_path = Rails.root.join('public/images/test_image.png')
+
+      # # # 画像選択フォームに画像を添付する
+      attach_file("post_facility[image]", image_path, make_visible: true)
+
+      # 送信するとPostモデルのカウントが1上がることを確認する
+
+      expect{
+      find('input[name="commit"]').click
+      sleep 1
+    }.to change {Post.count }.by(1)
+
+
+      expect(page).to have_content("ログイン")
+
+      visit root_path
+      sign_in(@user2)
+
+
+
+  #     # すでに投稿済みの内容がフォームに入っていることを確認する
+  #     expect(page).to have_xpath("//img[contains(@src, 'test_image')]")
+  #     expect(page).to have_field('名称', with: @post_facility.name)
+  #     expect(page).to have_field('住所', with: @post_facility.address)
+  #     expect(page).to have_field('コメント', with: @post_facility.content)
+
+  #     # 投稿内容を編集する
+  #     fill_in '名称', with: "#{@post_facility.name}+編集した名称"
+  #     fill_in '住所', with: "#{@post_facility.address}+編集した住所"
+  #     fill_in 'コメント', with: "#{@post_facility.content}+編集したコメント"
+
+  #     image_path2 = Rails.root.join('public/images/test_image2.png')
+
+  #     # # # 画像選択フォームに画像を添付する
+  #     attach_file("facility[image]", image_path2, make_visible: true)
+
+
+  #     # 編集してもTweetモデルのカウントは変わらないことを確認する
+  #     expect{
+  #     find('input[name="commit"]').click
+  #     sleep 1
+  #   }.to change { Post.count }.by(0)
+
+  #   visit post_path(latest_post_id)
+  #   expect(page).to have_xpath("//img[contains(@src, 'test_image2')]")
+  #   expect(page).to have_content("#{@post_facility.name}+編集した名称")
+    end
   end
- end
 end
