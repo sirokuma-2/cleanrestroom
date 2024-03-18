ada6624d8b7e69ba213c6ac266c312871dad5ce5
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-19 08:12:44 +0900

    model/post_facility_spec.rbの修正

--- spec/models/post_facility_spec.rb
+++ spec/models/post_facility_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe PostFacility, type: :model do
                                       powder_corner: facility.powder_corner,
                                       stroller_accessible: facility.stroller_accessible,
                                       image: facility.image)
-    sleep 0.2
+    sleep 0.5
   end
 
   describe '施設情報を登録できる場合' do
