72f4f459c58142bfdeb4e9ce1ce18b47a1689958
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-03-18 21:34:24 +0900

    database.ci.ymlのファイルの修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,20 +22,6 @@ require 'rspec/rails'
 #
 Rails.root.glob('spec/support/**/*.rb').sort.each { |f| require f }
 
-# Capybaraの設定
-Capybara.register_driver :selenium_chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1280,1024')
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-Capybara.javascript_driver = :selenium_chrome_headless
-
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
